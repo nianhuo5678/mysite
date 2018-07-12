ff6e9fd1fa9fda296072ae222036bc7c9cc1f875
nianhuo <nianhuo5678@163.com>
2018-07-12 22:58:09 +0800

    simple demo

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,19 +1,18 @@
 # chat/consumers.py
 from channels.generic.websocket import WebsocketConsumer
-import json
+import time
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
+        for i in range(10):
+            self.send(str(i))
+            time.sleep(1)
 
     def disconnect(self, close_code):
         pass
 
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        self.send(text_data)
 
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
